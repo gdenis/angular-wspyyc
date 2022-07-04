816a4de6e6d37793075afdfab643dd6b0f826f8e
gdenis <gabydll95@gmail.com>
2022-07-04 13:05:50 +0200

    5 files modified, 4 added

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Product } from './products';
 
@@ -6,6 +7,14 @@ import { Product } from './products';
 })
 export class CartService {
   items: Product[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  getShippingPrices() {
+    return this.http.get<{ type: string; price: number }[]>(
+      '/assets/shipping.json'
+    );
+  }
 
   addToCart(product: Product) {
     this.items.push(product);
@@ -19,6 +28,4 @@ export class CartService {
     this.items = [];
     return this.items;
   }
-
-  constructor() {}
 }
